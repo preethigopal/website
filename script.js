144a335eb816c68d13a2d7416968cd98d616d03e
preethi gopal <133478884+preethigopal@users.noreply.github.com>
2023-06-17 13:17:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal({ 
+    //reset: true,
+    distance : '80px',
+    duration : 2000,
+    delay: 200
+  });
+
+  ScrollReveal().reveal('.main', { origin: 'top' });
+  //ScrollReveal().reveal('.home-img img, .skill1, .achievements-container, .contact form ', { origin: 'bottom' });
+  //ScrollReveal().reveal('.home-content h1 ', { origin: 'left' });
+  //ScrollReveal().reveal('.home-content h3, .home-content p, .about-content ', { origin: 'right' });
